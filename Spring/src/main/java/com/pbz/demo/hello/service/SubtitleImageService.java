7ff119bc5e53b4ac0a3f5f8d57f5f0367327253e
unknown <yongling.huang@group6.io>
2020-12-15 00:53:10 -0800

    rgb:255,125,25

--- Spring/src/main/java/com/pbz/demo/hello/service/SubtitleImageService.java
+++ Spring/src/main/java/com/pbz/demo/hello/service/SubtitleImageService.java
@@ -42,9 +42,9 @@ public class SubtitleImageService {
 			SubtitleModel info = ls.get(i);
 			String strSubtitle = info.contextEng;
 			int nSecondsOfSubtitle = (info.end - info.star) / 1000;
-			int red = (int) (Math.random() * 255);
-			int green = (int) (Math.random() * 255);
-			int blue = (int) (Math.random() * 255);
+			int red 	= 255;//(int) (Math.random() * 255);
+			int green 	= 125;//(int) (Math.random() * 255);
+			int blue 	= 25;//(int) (Math.random() * 255);
 			for (int j = 0; j < nSecondsOfSubtitle; j++) {
 				String destImageFile = System.getProperty("user.dir") + "/" + Integer.toString(index + 1) + ".jpg";
 				BufferedImage image = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
